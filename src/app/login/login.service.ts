452e74797a912df659272b6bccf2b63bbae47288
Artur Piechula <artur.piechula@protonmail.com>
2018-11-27 20:18:29 +0100

    Move method to service.

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sendCredentials(url, username, password): void {
+    this.http.post(url, {'username': username, 'password': password}).subscribe( result => { console.log(result); },
+      error => console.log(error)
+      );
+  }
 }

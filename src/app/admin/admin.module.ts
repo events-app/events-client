e44c779c50b73b9d454f272dc6f3223934f2af66
Artur Piechula <artur.piechula@protonmail.com>
2019-05-20 20:31:02 +0200

    Add component for managing cards. Add styles for this component. Add routing.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { AdminComponent } from './admin.component';
 import { MatMenuModule, MatButtonModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ManageCardsComponent } from './manage-cards/manage-cards.component';
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, ManageCardsComponent],
   imports: [MatMenuModule,
             MatButtonModule,
             BrowserAnimationsModule],
---
+++ src/app/admin/manage-cards/manage-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-cards',
+  templateUrl: './manage-cards.component.html',
+  styleUrls: ['./manage-cards.component.scss']
+})
+export class ManageCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

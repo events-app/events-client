e2164c45df1308c1933de670fe8a00e8869f4dc1
Artur Piechula <artur.piechula@protonmail.com>
2019-05-31 20:27:19 +0200

    Add input and button for cards registering. Add method to register cards.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,12 +3,18 @@ import { AdminComponent } from './admin.component';
 import { MatMenuModule, MatButtonModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ManageCardsComponent } from './manage-cards/manage-cards.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AdminComponent, ManageCardsComponent],
   imports: [MatMenuModule,
             MatButtonModule,
-            BrowserAnimationsModule],
+            MatFormFieldModule,
+            BrowserAnimationsModule,
+            MatInputModule,
+            FormsModule],
   exports: [AdminComponent]
 })
 export class AdminModule { }
--- src/app/admin/manage-cards/manage-cards.component.ts
+++ src/app/admin/manage-cards/manage-cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-manage-cards',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageCardsComponent implements OnInit {
 
-  constructor() { }
+  public cardName: string;
+  private payload: any;
+
+  constructor(private http: HttpClient) { }
+
+  public registerCard(): any {
+    this.payload = {name: this.cardName, text: 'blabla'};
+    return this.http.post('http://localhost:8000/api/v1/cards', this.payload);
+  }
 
   ngOnInit() {
   }
